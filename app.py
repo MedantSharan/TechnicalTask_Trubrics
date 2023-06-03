d2b59594905074430bdb2213d6ad9067c25c336e
unknown <medsharanofficial@gmail.com>
2023-06-03 07:48:13 +0530

    Documentation

--- app.py
+++ app.py
@@ -13,14 +13,20 @@ def load_artifacts():
 
 #Create a function to handle feature inputs
 def make_inputs():
+   #Take user inputs and store them in variables 
    sepalWidth =  st.number_input("Sepal Width (cm)", step=0.1 )
    sepalLength = st.number_input("Sepal Length (cm)", step=0.1 )
    petalWidth =  st.number_input("Petal Width (cm)", step=0.1 )
    petalLength = st.number_input("Petal Length (cm)", step=0.1 )
+   #Return the input variables
    return petalWidth, petalLength, sepalWidth, sepalLength
 
+#Create a function to make a feedback system using Trubrics
 def deploy_thumbs_feedback():
+    #Use a feedback collector object to create a feedback system
     collector = FeedbackCollector(model="model")
+
+    #Store the feedback in a local variable and select the type of feedback system needed
     feedback = collector.st_feedback(feedback_type="thumbs")
     return feedback
 
@@ -28,13 +34,17 @@ def deploy_thumbs_feedback():
 
 def main():
     st.title("Iris Dataset ML Application")
+    #Add some information for the user to use the application
+    st.write("You can input the desired parameters and click on predict to see a prediction of the type of iris flower the input parameters may represent. After a prediction, you can use the thumbs-up and thumbs-down button to give feedback which is saved locally.")
     model, X_test, _ = load_artifacts()
     petalWidth, petalLength, sepalWidth, sepalLength = make_inputs()
 
+    #Make a button to make predictions based on data given by the user
     if st.button('Predict'):
         features = [sepalLength, sepalWidth, petalLength, petalWidth]
         prediction = model.predict([features])[0]
         st.write('Prediction:', prediction)
+    #Deploy the feedback system created
     feedback = deploy_thumbs_feedback()
 
 
